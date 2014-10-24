62d1041b64a71444b40f2b2a9c3da5827c85d445
ifollend <ivan.follender@hbo.com>
2014-10-24 08:04:48 -0400

    Add actual formula

--- git-getpull-homebrew-formula.rb
+++ git-getpull-homebrew-formula.rb
@@ -1,2 +1,14 @@
-#This is a simple homebrew formula for git-getpull
-#seed the create with brew create then use these contents.
+# This is a simple homebrew formula for git-getpull.
+# Seed the create with `brew create brew create https://github.com/DailyHandstand/git-getpull/archive/v0.9.tar.gz`
+# then use these contents.
+require "formula"
+
+class GitGetpull < Formula
+  homepage ""
+  url "https://github.com/DailyHandstand/git-getpull/archive/v0.9.tar.gz"
+  sha1 "17c153c32918474fab0d7cd728cd6543d69bad79"
+
+  def install
+    bin.install "git-getpull" => "git-getpull"
+  end
+end
